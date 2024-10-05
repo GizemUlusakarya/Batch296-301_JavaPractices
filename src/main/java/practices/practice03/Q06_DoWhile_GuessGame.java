9dfe3191629387bbd71f919f339c1316058f6656
GizemUlusakarya <gizem.techpro@gmail.com>
2024-10-05 19:53:16 +0100

    v1

---
+++ src/main/java/practices/practice03/Q06_DoWhile_GuessGame.java
@@ -0,0 +1,15 @@
+package practices.practice03;
+
+public class Q06_DoWhile_GuessGame {
+    public static void main(String[] args) {
+
+        /*
+        HOMEWORK
+        Type code for a game that finds random numbers between 0 and 100
+        Hint:
+           If the number you entered is less than random number, print => Enter a greater number
+           If the number you entered is greater than random number, print => Enter a smaller number
+           If the number you entered is equal to the random number, print => Congratulations! You found the number
+         */
+    }
+}
